4e1f9a8875341f2489946f39523347ee2f1b568c
Daniel Molina <danielmolina.dev@gmail.com>
2025-05-22 15:58:37 +0200

    feat: add wallet providers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+
 export default function Home() {
-  return <div>GM!</div>;
+  return (
+    <div>
+      GM!
+      <ConnectButton />
+    </div>
+  );
 }
